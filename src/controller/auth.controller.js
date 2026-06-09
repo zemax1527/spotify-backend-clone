98ea677963f54067a4b9c9421b6c9d6dafe34207
Bhavesh Talande <bhaveshtalande@gmail.com>
2026-06-09 17:39:00 +0530

    Created login api

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require('../model/user.model') 
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
  async function registerUser(req, res) {
     const { username, email, password, role ='user'} = req.body
@@ -17,10 +18,12 @@ const jwt = require('jsonwebtoken')
         })
     }
 
+    const hash = await bcrypt.hash(password, 10)
+
     const user = await userModel.create({
         username,
         email,
-        password,
+        password : hash,
         role
     })
 
@@ -41,6 +44,45 @@ const jwt = require('jsonwebtoken')
     })
 }
 
+async function loginUser(req, res) {
+    const { username, email, password } = req.body
 
+    const user = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
 
-module.exports = {registerUser}
+    if(!user){
+        return res.status(401).json({
+            message : 'Invalid crdientials'
+        })
+    }
+
+    const isPasswordCorrect = await bcrypt.hash(password, user.password)
+
+    if(!isPasswordCorrect){
+        return res.status(401).json({
+            message : 'Invalid password'
+        })
+    }
+
+    const token = jwt.sign({
+        id : user._id,
+        role: user.role
+    },process.env.JWT_SECRET)
+
+    res.cookie('token', token)
+    res.status(200).json({
+        message : 'User login successfully',
+          user : {
+            id : user.id,
+            username : user.username,
+            email : user.email,
+            role : user.role
+        }
+    })
+}
+
+module.exports = {registerUser , loginUser}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const authControl = require('../controller/auth.controller')
 
 router.post('/register', authControl.registerUser)
+router.post('/login', authControl.loginUser)
 
 module.exports = router
